9648187afb9ffcf7c0c58611da7f265b62c4820b
vishnukvnvv <vishnukvnvv@gmail.com>
2024-10-22 18:21:51 +0530

    remove image view

--- image.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn").addEventListener('click', (event) => {
-    let url = document.getElementById("image").value;
-    fetch(url, { method: 'GET' })
-        .then((res) => res.blob()) // Gets the response and returns it as a blob
-        .then((blob) => {
-            const dataURL = URL.createObjectURL(new Blob([blob]));
-            document.getElementById("load").src = dataURL
-        }).catch((err) => console.log(err));
-});
